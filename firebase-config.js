743a702d63c761985fa1e9040a2d9770c9f00b6b
ds9453285949-eng <ds9453285949@gmail.com>
2026-06-04 10:30:35 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,20 @@
+```javascript
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyDJSmirvoIwieTr25NQUOtAHF-v_aTINoM",
+authDomain: "godlinsta.firebaseapp.com",
+projectId: "godlinsta",
+storageBucket: "godlinsta.firebasestorage.app",
+messagingSenderId: "808932933868",
+appId: "1:808932933868:web:098b5e9cb0fd4e856065c9"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+```
 
